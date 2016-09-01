e14367826257863d54ce2a5efa4bfeb259b9695a
Andrea Levy <alevy05@calpoly.edu>
2016-09-01 14:59:36 -0700

    Adding application files

---
+++ app/src/main/java/com/example/andrea/notes/EditNote.java
@@ -0,0 +1,58 @@
+package com.example.andrea.notes;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+
+
+public class EditNote extends AppCompatActivity implements TextWatcher {
+
+    public EditText editText;
+    private int noteId;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_note);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        editText = (EditText) findViewById(R.id.editText);
+        Intent i = getIntent();
+        noteId = i.getIntExtra("noteId", -1);
+        if (noteId != -1)
+        {
+           editText.setText(MainActivity.list.get(noteId));
+        }
+        editText.addTextChangedListener(this);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int count, int after) {
+        MainActivity.list.set(noteId, String.valueOf(s));
+        MainActivity.stringArrayAdapter.notifyDataSetChanged();
+        MainActivity.set.clear();
+        MainActivity.set.addAll(MainActivity.list);
+        SharedPreferences sharedPreferences = this.getSharedPreferences("com.example.andrea.notes", Context.MODE_PRIVATE);
+        sharedPreferences.edit().remove("list").apply();
+        sharedPreferences.edit().putStringSet("list", MainActivity.set).apply();
+    }
+
+    @Override
+    public void afterTextChanged(Editable editable) {
+
+    }
+}
---
+++ app/src/main/java/com/example/andrea/notes/MainActivity.java
@@ -0,0 +1,135 @@
+package com.example.andrea.notes;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static ArrayList<String> list = new ArrayList<>();
+    public static ArrayAdapter<String> stringArrayAdapter;
+    static Set<String> set;
+    private SharedPreferences sharedPreferences;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ListView listView = (ListView) findViewById(R.id.notes_list);
+
+        sharedPreferences = this.getSharedPreferences("com.example.andrea.notes", Context.MODE_PRIVATE);
+        set = sharedPreferences.getStringSet("list", null);
+        if (set != null) {
+            list.clear();
+            list.addAll(set);
+        }
+        else {
+            list.add("Sample Note");
+            set = new HashSet<>();
+            set.addAll(list);
+            updateSharedPreferences();
+        }
+
+
+
+        stringArrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,list);
+
+        listView.setAdapter(stringArrayAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent i = new Intent(getApplicationContext(), EditNote.class);
+                i.putExtra("noteId", position);
+                startActivity(i);
+
+            }
+        });
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id) {
+                new AlertDialog.Builder(MainActivity.this)
+                        .setIcon(android.R.drawable.ic_delete)
+                        .setTitle("Are you sure you want to delete this note?")
+                        .setMessage("Click yes to delete this note")
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener(){
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int which) {
+                                list.remove(position);
+                                updateSet();
+                                set.addAll(list);
+
+                                stringArrayAdapter.notifyDataSetChanged();
+                            }
+                        }).setNegativeButton("No", null)
+                        .show();
+                return true;
+            }
+        });
+
+
+    }
+
+    private void updateSharedPreferences()
+    {
+        sharedPreferences.edit().remove("list").apply();
+        sharedPreferences.edit().putStringSet("list", set).apply();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.add_note) {
+            list.add("");
+            updateSet();
+            Intent i = new Intent(getApplicationContext(), EditNote.class);
+            i.putExtra("noteId",list.size() - 1);
+            startActivity(i);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void updateSet()
+    {
+        if (set == null) {
+            set = new HashSet<>();
+        }
+        else {
+            set.clear();
+        }
+        set.addAll(list);
+        updateSharedPreferences();
+    }
+}
